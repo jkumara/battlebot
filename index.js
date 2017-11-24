8089d02b7fa32113ab4a76015389335c12274b89
Aki Ryynänen <Aki.Ryynanen@solita.fi>
2017-11-24 19:00:11 +0200

    base

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
 const http = require('http')
+const R = require('ramda')
 const [port] = process.argv.slice(2)
+
+const moveTypes = {
+  MOVE: 'move',
+  BOMB: 'bomb',
+  NOOP: 'noop'
+}
+
+let moves = []
+const nextMove = () => R.pipe(
+  R.takeLast(2, moves),
+  ([first, second]) => {
+    return first === moveTypes.MOVE || second === moveTypes.MOVE ? moveTypes.BOMB : moveTypes.MOVE
+  }
+)(moves)
 
 const random = (min, max) => Math.floor(Math.random() * (max - min + 1) + min)
 
@@ -9,13 +24,49 @@ if (!port) {
 }
 
 const NOOP = {task: 'NOOP'}
+const MOVE = {task: 'MOVE'}
+const BOMB = {task: 'BOMB'}
+
+const whatNext = () => {
+  const nextMove = nextMove()
+  moves.push(nextMove)
+  return nextMove
+}
+
+const move = () => {
+  return MOVE
+}
+
+const bomb = () => {
+  return BOMB
+}
+
+const actionsPerTick = ({numOfTasksPerTick}, players, items) => {
+  let retVal = []
+  for(let i = 0; i < numOfTasksPerTick; ++i) {
+    const move = whatNext()
+    switch(move) {
+      case moveTypes.MOVE:
+        retVal.push(move())
+        break:
+      case moveTypes.BOMB:
+        retVal.push(bomb())
+        break:
+      case moveTypes.NOOP:
+      default:
+        retVal.push(NOOP)
+    }
+  }
+
+  return retVal
+}
 
 const playTurn = ({gameInfo, players, items}) => {
   if (!gameInfo) {
     return [NOOP]
   }
 
-  return Array(gameInfo.numOfTasksPerTick).fill(NOOP)
+  return actionsPerTick(gameInfo, players, items)
 }
 
 
